07f4f1d90b3c98be2682bcaf2cb35bcefc9df024
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-06-08 20:01:41 -0300

    Implentar cadastro e login de usuário

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ app.use(express.urlencoded({extended: true}));
 
 //Database
 database.authenticate()
-    .then(()=>console.log('Banco de dados conectado!'))
+    .then(()=> {
+        console.log('Banco de dados conectado!')
+    })
     .catch(error => {
         console.log(`Erro ao conectar com banco de dados: ${error}`)
     });
 
+//Routes
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
@@ -30,9 +33,46 @@ app.get('/register', (req, res) => {
     res.render('register.ejs');
 });
 
+app.post('/user/new', (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.create({
+        name, 
+        email,
+        password
+    }).then(() => {
+        req.redirect('/login');
+    }).catch(error => {
+        console.log(`Erro ao cadastrar novo usuário: ${error}`);
+        res.redirect('/login');
+    });
+});
+
+app.post('/user/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findOne({
+        where: {email}
+    }).then(user => {
+        if(user && user.password === password){
+            
+            res.render('admin/user/index.ejs', {
+                user
+            });
+
+        }else
+            res.redirect('/login');
+    }).catch(() => {
+        res.redirect('/login');
+    });
+});
+
 app.listen('8080', '127.0.0.1', error => {
     if(error)
         console.log(`Erro ao iniciar servidor: ${error}`);
-    else 
+    else
         console.log('Servidor iniciado!');
 });
